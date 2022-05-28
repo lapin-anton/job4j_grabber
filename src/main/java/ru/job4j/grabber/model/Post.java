469b1c1f9419deb17b8a31cc0eba5a768d143b6c
anton-lapin <lapin_antoine@mail.ru>
2022-05-28 22:15:28 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -60,14 +60,14 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id
-                && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link);
+        return Objects.equals(title, post.title)
+                && Objects.equals(link, post.link)
+                && Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link);
+        return Objects.hash(title, link, created);
     }
 
     @Override
